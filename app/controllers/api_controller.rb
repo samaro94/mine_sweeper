039b8cfd8b0805caac8feb3444f641b9a251c832
samaro <seal_111194@hotmail.com>
2022-02-21 16:40:44 -0600

    mines sweeper controller a route

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,53 @@
+class ApiController < ApplicationController
+  before_action :get_mines_map
+
+  def solution
+    solve_mines_map
+
+    render json: {
+        problem: @problem,
+        # problem: @problem.each{|x| puts x.inspect},
+        solution: @solution
+    }
+
+  end
+
+  private
+
+  def get_mines_map
+    response = HTTParty.get(
+        "https://mine-sweeper-generator.herokuapp.com/solver",
+        headers: {
+            'Content-Type' => "application/json"
+        }
+    )
+    @problem = JSON.parse(response.parsed_response) if !response.blank? && response.code == 200
+  end
+
+  def solve_mines_map
+    return if @problem.blank?
+    @solution = Marshal.load(Marshal.dump(@problem))
+
+    for i in 0..@problem.size-1 do
+      for j in 0..@problem.first.size-1 do
+        puts "#{i} - #{j}"
+        if @problem[i][j].blank?
+
+          mines = []
+          mines.push(@problem[i-1][j-1])
+          mines.push(@problem[i-1][j])
+          mines.push(@problem[i-1][j+1])
+          mines.push(@problem[i][j-1])
+          mines.push(@problem[i][j+1])
+          mines.push(@problem[i+1][j-1])
+          mines.push(@problem[i+1][j])
+          mines.push(@problem[i+1][j+1])
+
+          @solution[i][j] = mines.count{|x| x=="*"}
+        end
+      end
+    end
+
+  end
+
+end
